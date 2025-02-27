c1d513922fa60a1e5beebe22209aac556683e3c3
Eugene Dunn <134093002+QSC-Eugene@users.noreply.github.com>
2025-02-26 22:45:13 -0500

    Update info.lua

--- info.lua
+++ info.lua
@@ -4,5 +4,5 @@ PluginInfo = {
   BuildVersion = "1.3.1.1",
   Version = "1.3.1.1",
   Id = "333c8f3d-a3e4-46fe-9c44-d76e3d95d188",
-  Description = ""
+  Description = "A drop in replacement for a Router Component that utilizes internal crossfaders"
 }
