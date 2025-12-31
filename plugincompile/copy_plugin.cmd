19860385974216e488ca500dabb8103dd5a54fb5
Eugene Dunn <eugenedunn@eugenestech.com>
2025-12-31 11:21:23 -0500

    fixed copy

--- plugincompile/copy_plugin.cmd
+++ plugincompile/copy_plugin.cmd
@@ -1,18 +1,5 @@
 :: %1 workspaceFolder             C:\qsys_plugins\plugin
 :: %2 workspaceFolderBasename     plugin
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`WhoAmI`) DO (
-SET var=%%F
-)
-if %var% == desktop-k3b4pl2\eboy9 goto :EugenePC else goto :AnyOther
-
-:AnyOther
-::if not exist "%userprofile%\Documents\QSC\Q-Sys Designer\Plugins\%2" mkdir "%userprofile%\Documents\QSC\Q-Sys Designer\Plugins\%2"
-COPY /Y "%1\%2.qplug" "%userprofile%\Documents\QSC\Q-Sys Designer\Plugins\%2.qplug"
-
-exit 0
-
-:: For My Computer
-:EugenePC
-::if not exist "H:\eboy9\Documents\QSC\Q-Sys Designer\Plugins\%2" mkdir "H:\eboy9\Documents\QSC\Q-Sys Designer\Plugins\%2"
-COPY /Y "%1\%2.qplug" "H:\eboy9\Documents\QSC\Q-Sys Designer\Plugins\%2.qplug" 
+FOR /F "tokens=*" %%g IN ('powershell "(new-object -COM Shell.Application).Namespace(0x05).Self.Path"') do (SET DocumentsFolder=%%g)
+COPY /Y "%~1\%~2.qplug" "%userprofile%\Documents\QSC\Q-Sys Designer\Plugins\%~2.qplug"
